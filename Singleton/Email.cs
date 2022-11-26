b755a24254407eb7ae5e27a6057c8c74b350be80
bruno <bruno@MORAVEC>
2022-11-26 17:25:11 -0300

    Adicionar arquivos de projeto.

---
+++ Singleton/Email.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Email
+    {
+        private Email() { } //construtor privado
+
+        private static Email instancia; //atributo para armazenamento da instância
+
+        public static Email GetInstancia() //método pra acessar o construtor
+        {
+            if(instancia == null) //verifica se a instância já foi inicializada
+            {
+                instancia = new Email(); // se ainda não, inicia 
+            }
+            return instancia; //retorna a instância
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Progam
+    {
+        public static void Main(string[] args)
+        {
+            Email E1 = Email.GetInstancia();
+            Email E2 = Email.GetInstancia();
+
+            if (E1 == E2)
+            {
+                Console.WriteLine("Instancias iguais");
+            }
+            else
+            {
+                Console.WriteLine("Instancias Diferentes");
+            }
+        }
+    }
+}
